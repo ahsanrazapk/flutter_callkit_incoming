e2305808c10f52b7fdd01263fa9693acbcc91588
ahsanrazatco <ahsan.raza.tco@gmail.com>
2025-09-23 12:11:33 +0500

    dev

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/ApiClient.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/ApiClient.kt
@@ -9,7 +9,7 @@ object ApiClient {
 
     val instance: CallApiService by lazy {
         Retrofit.Builder()
-            .baseUrl("https://dev-be.legalavenue.pk/")
+            .baseUrl("https://prod-be.legalavenue.pk/")
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
